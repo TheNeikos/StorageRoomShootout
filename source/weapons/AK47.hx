7c436707068aa813eeed260653972f2f37e4459c
Neikos <neikos@neikos.email>
2021-07-20 20:01:39 +0200

    Add AK47 shot sounds

--- source/weapons/AK47.hx
+++ source/weapons/AK47.hx
@@ -2,9 +2,12 @@ package weapons;
 
 import flixel.FlxSprite;
 import flixel.math.FlxPoint;
+import flixel.system.FlxSound;
 
 class AK47 extends Weapon
 {
+	var shotSound:FlxSound;
+
 	public function new()
 	{
 		var icon = new FlxSprite().loadGraphic(AssetPaths.entities__png, true, 16, 16);
@@ -13,11 +16,18 @@ class AK47 extends Weapon
 		damage = 8;
 		speed = 600;
 		shotCooldown = 0.12;
+		shotSound = FlxG.sound.load(AssetPaths.gunshot__ogg);
 	}
 
 	override function createBullet(origin:FlxPoint, angle:Float, num:Int):Bullet
 	{
 		angle += FlxG.random.int(-5, 5);
 		return super.createBullet(origin, angle, num);
+	}
+
+	override function doShoot(origin:FlxPoint, angle:Float)
+	{
+		super.doShoot(origin, angle);
+		shotSound.play(true);
 	}
 }
